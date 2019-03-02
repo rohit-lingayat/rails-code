3fbd0091a0c0070a69a490b983871ce551e2a9d4
rohit-webonise <rohit.lingayat@weboniselab.com>
2019-03-03 01:07:31 +0530

    Fix test cases and React integration

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,18 @@
 class Student < ActiveRecord::Base
+  scope :filter, lambda { |movie|
+    return if movie.nil?
+    where('Lower(favoritemovie) =?', movie)
+  }
+
+  def name
+    firstname
+  end
+
+  def movie
+    favoritemovie
+  end
+
+  def name_with_initial
+    "#{firstname} #{lastname[0]}."
+  end
 end
